b730da83d469a4525aa8c08e25172f8bf3612345
Erik Banás <erikghast@gmail.com>
2022-09-28 19:56:04 +0200

    Update

---
+++ Assets/PlayerMouseInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseInput : MonoBehaviour
+{
+    public Animator animator;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
